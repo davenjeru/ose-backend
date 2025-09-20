113b069a92f1691f0ff8058f9b6bb36ce1d0fb61
Dave Mathews Njeru <41262970+davenjeru@users.noreply.github.com>
2025-09-20 19:50:58 +0300

    build: vercel experimentation (#3)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,7 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ConfigModule } from '@nestjs/config';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
-import { join } from 'path';
 import { PrismaModule } from './prisma/prisma.module';
 import { HealthModule } from './health/health.module';
 import { NewsletterModule } from './newsletter/newsletter.module';
@@ -15,16 +14,12 @@ import { MessagesModule } from './messages/messages.module';
     }),
     GraphQLModule.forRootAsync<ApolloDriverConfig>({
       driver: ApolloDriver,
-      inject: [ConfigService],
-      useFactory: (configService: ConfigService) => {
-        const nodeEnv = configService.get('NODE_ENV') || 'development';
-        const isDevelopment = nodeEnv === 'development';
-
+      useFactory: () => {
         return {
-          autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+          autoSchemaFile: true,
           sortSchema: true,
-          playground: isDevelopment,
-          introspection: isDevelopment,
+          playground: true,
+          introspection: true,
         };
       },
     }),
